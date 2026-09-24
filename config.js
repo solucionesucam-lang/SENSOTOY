c3441b70fa442cc67c480f9cb4509c22701dfd62
solucionesucam-lang <solucionesucam@gmail.com>
2026-09-24 13:37:12 +0200

    Update config.js

--- config.js
+++ config.js
@@ -9,5 +9,5 @@
 // NUNCA pongas aquí la clave "secret" ni la "service_role":
 // saltan toda la seguridad y quedarían visibles en GitHub.
 // =====================================================================
-export const SUPABASE_URL = 'qzfiingwkfedyhdpmatl';
+export const SUPABASE_URL = 'https://qzfiingwkfedyhdpmatl.supabase.co';
 export const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF6Zmlpbmd3a2ZlZHloZHBtYXRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAyMzc0MTUsImV4cCI6MjEwNTgxMzQxNX0.-o89Qv6Y7WNtBxdddN4t3Q96xNXDgRC1uVe8m6Fbmfo';
